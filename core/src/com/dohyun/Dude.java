ee6fc5ec15c9f63cd971b400bd81e0618299f3d9
dh12091209 <83049395+dh12091209@users.noreply.github.com>
2022-11-08 07:32:02 -0800

    Update Dude.java

--- core/src/com/dohyun/Dude.java
+++ core/src/com/dohyun/Dude.java
@@ -8,6 +8,7 @@ import java.util.Stack;
 
 public class Dude {
     private Location loc;
+    private Location startLoc;
     private WumpusWorld myWorld;
     private Texture texture;
     private boolean HasGold = false;
@@ -19,48 +20,62 @@ public class Dude {
 
 
     public Dude(Location loc, WumpusWorld myWorld){
-        this.loc = loc;
+        //this.loc = loc;
+        this.startLoc = loc;
         this.myWorld = myWorld;
-        stack.push(new Location(loc.getRow(),loc.getCol()));
         texture = new Texture("guy.png");
+        reset(loc);
+
+        /*
+        this.loc = new Location(loc.getRow(),loc.getCol());
         myWorld.makeVisible(loc);
         visited = new boolean[10][10];
         visited[9][0]=true;
-        System.out.println("Stack: " + stack);
+        stack.push(new Location(loc.getRow(),loc.getCol()));
+        System.out.println("Initial Stack: " + stack);
+        */
     }
 
     public boolean KilledWumpus(){
         return killedWumpus;
     }
-    public void moveRight(){
+    public boolean moveRight(){
         if(loc.getCol()+1 < myWorld.getNumCols()) {
             loc.setCol(loc.getCol() + 1);
             myWorld.makeVisible(loc);
             totalSteps ++;
+            return true;
         }
+        return false;
     }
-    public void moveLeft() {
+    public boolean moveLeft() {
         if(loc.getCol()-1 >= 0) {
             loc.setCol(loc.getCol() - 1);
             myWorld.makeVisible(loc);
             totalSteps ++;
+            return true;
         }
+        return false;
     }
 
-    public void moveDown() {
+    public boolean moveDown() {
         if(loc.getRow()+1 < myWorld.getNumRows()) {
             loc.setRow(loc.getRow() + 1);
             myWorld.makeVisible(loc);
             totalSteps ++;
+            return true;
         }
+        return false;
     }
 
-    public void moveUp() {
+    public boolean moveUp() {
         if(loc.getRow()-1 >= 0) {
             loc.setRow(loc.getRow() - 1);
             myWorld.makeVisible(loc);
             totalSteps ++;
+            return true;
         }
+        return false;
     }
     public void draw(SpriteBatch spriteBatch){
         Point myPoint = myWorld.convertRowColToCoords(loc);
@@ -70,10 +85,13 @@ public class Dude {
         return loc;
     }
     public void reset(Location loc){
-        this.loc = loc;
+        //this.loc = loc;
+        this.loc = new Location(loc.getRow(),loc.getCol());
         myWorld.makeVisible(loc);
         visited = new boolean[10][10];
         visited[9][0]=true;
+        stack.clear();
+        stack.push(new Location(loc.getRow(), loc.getCol()));
     }public void setTotalSteps(){
         totalSteps = 0;
     }
@@ -88,27 +106,49 @@ public class Dude {
         HasGold = hasGold;
     }
     public void randomAISolution(){
+
+//        System.out.println("------------");
+
+        boolean isMoved = false;
         int choice = (int)(1+Math.random() * 4);
         if(choice==1){
-            moveDown();
+            isMoved = moveDown();
+            //System.out.println("Down " + loc);
         }else if(choice==2){
-            moveLeft();
+            isMoved = moveLeft();
+            //System.out.println("Left " + loc);
         }else if(choice==3){
-            moveRight();
+            isMoved = moveRight();
+            //System.out.println("Right " + loc);
         }else {
-            moveUp();
+            isMoved = moveUp();
+            //System.out.println("Up " + loc);
         }
-        System.out.println("------------");
-        System.out.println("Stack: " + stack);
-        System.out.println(stack.size());
-        stack.push(new Location(loc.getRow(),loc.getCol()));
-        System.out.println("loc: " + loc.toString() + "stack: " + stack.lastElement().toString());
+        // push only when it actually moved
+        if (isMoved) {
+            stack.push(new Location(loc.getRow(),loc.getCol()));
+            //System.out.println("pushed : " + loc.toString());
+        }
+
+        //System.out.println("Stack: " + stack);
+//        System.out.println("Stack size: " + stack.size());
+//        System.out.println("Current loc: " + loc.toString()); // + "stack: " + stack.lastElement().toString());
+        /*
         if(myWorld.getTileId(loc) == WumpusWorld.WUMPUS||myWorld.getTileId(loc) == WumpusWorld.PIT||myWorld.getTileId(loc) == WumpusWorld.SPIDER){//fix it
             stack.pop();
             loc = stack.lastElement();
         }
-        else if(visited[loc.getRow()][loc.getCol()]&& stack.get(0) != loc){
-            stack.pop();
+        */
+        //else if(visited[loc.getRow()][loc.getCol()]&& stack.get(0) != loc){
+        if(isMoved && visited[loc.getRow()][loc.getCol()]){
+            if (!stack.empty()) { 
+                Location poped = stack.pop();
+                //loc = stack.lastElement();
+                loc = new Location(stack.lastElement().getRow(), stack.lastElement().getCol());
+                //System.out.println("popped : " + poped.toString());
+                //System.out.println("loc changed: " + loc.toString());
+            }
+            /*
             if(choice==1){
                 moveUp();
             }else if(choice==2){
@@ -118,9 +158,10 @@ public class Dude {
             }else {
                 moveDown();
             }
+            */
             totalSteps--;
             }
-        System.out.println("------------");
+//        System.out.println("------------");
 
 
     }
@@ -135,9 +176,20 @@ public class Dude {
                 return false;
             }
         }
-        if(noway && !loc.equals(stack.get(0))){
-            stack.pop();
-            loc = stack.lastElement();
+        //if(noway && !loc.equals(stack.get(0))){
+        if(noway){
+            if (!stack.empty()) stack.pop();
+
+            // update location
+            if (!stack.empty()) {
+                loc = new Location(stack.lastElement().getRow(), stack.lastElement().getCol());
+            }
+            else {
+                loc = new Location(startLoc.getRow(),startLoc.getCol());
+                stack.push(new Location(loc.getRow(),loc.getCol()));
+            }
+//            System.out.println("Noway, loc = " + loc.toString());
+
         }
         return true;
     }
@@ -153,17 +205,24 @@ public class Dude {
 
             }
             else if(myWorld.getTileId(loc) == WumpusWorld.WEB||myWorld.getTileId(loc) == WumpusWorld.WIND||myWorld.getTileId(loc) == WumpusWorld.STINK){
-                stack.pop();
-                loc = stack.lastElement();
-                randomAISolution();
-
+                if (!stack.empty()) {
+                    stack.pop();
+                    //loc = stack.lastElement();x
+                    loc = new Location(stack.lastElement().getRow(), stack.lastElement().getCol());
+                    //randomAISolution();
+                }
+                else {
+                    loc = new Location(startLoc.getRow(),startLoc.getCol());;
+                    stack.push(new Location(loc.getRow(), loc.getCol()));
+                }
+//                System.out.println("Stink, loc = " + loc.toString());
 
             }else {
                 randomAISolution();
-                System.out.println(stack.lastElement().toString());
-                if (myWorld.getTileId(loc) == WumpusWorld.GROUND && !visited[loc.getRow()][loc.getCol()]) {
-                    stack.push(new Location(loc.getRow(), loc.getCol()));
-                }
+                //System.out.println(stack.lastElement().toString());
+                //if (myWorld.getTileId(loc) == WumpusWorld.GROUND && !visited[loc.getRow()][loc.getCol()]) {
+                //    stack.push(new Location(loc.getRow(), loc.getCol()));
+                //}
             }
             visited[loc.getRow()][loc.getCol()] = true;
         }
